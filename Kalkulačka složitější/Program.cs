03d77335e9a90d6e01b28be561cc5a944a618b15
Hasák Ondřej <Hasak.On.2022@skola.ssps.cz>
2022-09-27 09:43:47 +0200

    first commit

---
+++ Kalkulačka složitější/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulačka_složitější
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cislo1, cislo2, vysledek = 0;
+            string operace;
+
+            cislo1 = double.Parse(Console.ReadLine());
+            cislo2 = double.Parse(Console.ReadLine());
+            operace = Console.ReadLine();
+
+            if (operace == "+")
+            {
+                vysledek = cislo1 + cislo2;
+            }
+            else if (operace == "-")
+            {
+                vysledek = cislo1 - cislo2;
+            }
+            else if (operace == "*")
+            {
+                vysledek = cislo1 * cislo2;
+            }
+            else if (operace == "/")
+            {
+                vysledek = cislo1 / cislo2;
+            }
+
+            else
+            {
+                Console.WriteLine("Špatná operace");
+            }
+
+            Console.WriteLine("Výsledek: " + vysledek);
+            Console.ReadKey();
+        }
+    }
+}
